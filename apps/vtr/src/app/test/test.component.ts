8bd647361f5365344512427ff9d3454343b50d97
Nayak-lipika <lipikanayak.b@prodapt.com>
2020-11-15 20:26:41 +0530

    first commit

---
+++ apps/vtr/src/app/test/test.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core'; 
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'lla-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss']
+})
+export class TestComponent implements OnInit {
+
+  sampleData : {} = {};
+  constructor( private http: HttpClient ) { }
+
+  ngOnInit(): void {
+
+    this.http.get("https://directus.projects.prodaptconsulting.com/directus/items/basic_pages/1")
+    .subscribe((response : {})=>{
+        this.sampleData = response;
+        console.log(this.sampleData);
+    })
+
+   
+  }
+
+}
